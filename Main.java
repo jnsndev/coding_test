165b60a19b85963a6792fc8a15d04807ccbde86c
jnsndev <jnsn.yang@gmail.com>
2021-05-26 19:59:26 +0900

    [DFS] 조합 구하기

--- Main.java
+++ Main.java
@@ -1,45 +1,31 @@
 import java.util.*;
 
 /**
- * 미로탐색 (DFS)
+ * 조합 구하기
  */
 public class Main {
-    static int[][] miro = new int[8][8];
-    // index 0: 좌 1: 상 2: 우 2: 하
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
-    static int answer = 0;
+    static int n, m;
+    static int[] combination, chk;
 
-    public void DFS(int x, int y) {
-        if (x == 7 && y == 7) answer++;
-        else {
-            // 현재좌표의 상하좌우 4방향 검사
-            for (int i=0; i<4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx >=1 && nx <=7 && ny >= 1 && ny <=7 && miro[nx][ny] == 0) {
-                    miro[nx][ny] = 1;
-                    DFS(nx, ny);
-                    miro[nx][ny] = 0;
-                }
+    public void DFS(int L, int s) {
+        if (L == m) {
+            for (int x : combination) System.out.print(x + " ");
+            System.out.println();
+        } else {
+            for (int i=s; i<=n; i++) {
+                combination[L] = i;
+                DFS(L+1, i+1);
             }
         }
     }
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-
-        for (int i=1; i<=7; i++) {
-            for (int j=1; j<=7; j++) {
-                miro[i][j] = kb.nextInt();
-            }
-        }
+        n = kb.nextInt();
+        m = kb.nextInt();
         kb.close();
-        miro[1][1] = 1;
-
-        // 출발점 좌표 (x: 1, y: 1)
-        T.DFS(1, 1);
-        System.out.println(answer);
+        chk = new int[n+1];
+        combination = new int[m];
+        T.DFS(0, 1);
     }
 }
