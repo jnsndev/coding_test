8b10771be1c8bcafb8ba1a0802e674645d762d4d
jnsndev <jnsn.yang@gmail.com>
2021-06-01 07:24:30 +0900

    [DFS] 재귀함수

--- Main.java
+++ Main.java
@@ -1,25 +1,21 @@
 import java.util.*;
  
 /**
- * 대소문자 변환 (아스키코드 이용)
+ * 재귀함수
  */ 
 public class Main {
-    public String solution(String str) {
-        String answer = "";
-
-        for (int x : str.toCharArray()) {
-            // 대문자 아스키코드 : 65 ~ 90, 소문자 아스키코드 : 97 ~ 122
-            if (x >= 97 && x <= 122) answer += (char) (x-32);
-            else answer += (char) (x+32);
+    public void DFS(int n) {
+        if (n == 0) return;
+        else {
+            DFS(n-1);
+            System.out.print(n + " ");
         }
-
-        return answer;
     }
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        String str = kb.next();
+        int n = kb.nextInt();
         kb.close();
-        System.out.println(T.solution(str));
+        T.DFS(n);
     }
 }
