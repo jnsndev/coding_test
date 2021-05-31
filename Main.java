c7a106a7226bc0b681499c05880498995fe33933
jnsndev <jnsn.yang@gmail.com>
2021-06-01 08:16:47 +0900

    [String] 문장 속 가장 긴 단어 찾기 (split 사용)

--- Main.java
+++ Main.java
@@ -1,21 +1,28 @@
 import java.util.*;
  
 /**
- * 재귀함수
+ * 문장 속 가장 긴 단어 찾기 (split 사용)
  */ 
 public class Main {
-    public void DFS(int n) {
-        if (n == 0) return;
-        else {
-            DFS(n-1);
-            System.out.print(n + " ");
+    public String solution(String str) {
+        String[] arr = str.split(" ");
+        String answer = "";
+        int cnt = Integer.MIN_VALUE;
+
+        for (String s : arr) {
+            if (cnt < s.length()) {
+                cnt = s.length();
+                answer = s;
+            }
         }
+
+        return answer;
     }
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
+        String str = kb.nextLine();
         kb.close();
-        T.DFS(n);
+        System.out.println(T.solution(str));
     }
 }
