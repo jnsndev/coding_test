1b47eb6deb501e7724bef291b05d33b0ab52855b
jnsndev <jnsn.yang@gmail.com>
2021-06-01 07:02:22 +0900

    [String] 대소문자 변환 (아스키코드 이용)

--- Main.java
+++ Main.java
@@ -1,15 +1,16 @@
 import java.util.*;
  
 /**
- * 대소문자 변환
+ * 대소문자 변환 (아스키코드 이용)
  */ 
 public class Main {
     public String solution(String str) {
         String answer = "";
 
-        for (char x : str.toCharArray()) {
-            if (Character.isLowerCase(x)) answer += Character.toUpperCase(x);
-            else answer += Character.toLowerCase(x);
+        for (int x : str.toCharArray()) {
+            // 대문자 아스키코드 : 65 ~ 90, 소문자 아스키코드 : 97 ~ 122
+            if (x >= 97 && x <= 122) answer += (char) (x-32);
+            else answer += (char) (x+32);
         }
 
         return answer;
