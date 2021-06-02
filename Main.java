bbba3936bf8faa24b0179aaa7df58e659c4d340e
jnsndev <jnsn.yang@gmail.com>
2021-06-02 22:42:27 +0900

    [String] 숫자만 추출 (아스키코드)

--- Main.java
+++ Main.java
@@ -1,22 +1,23 @@
 import java.util.*;
  
 /**
- * 유효한 팰린드롬 (replaceAll)
+ * 숫자만 추출 (아스키코드)
  */ 
 public class Main {
-    public String solution(String str) {
-        String answer = "NO";
-        str = str.toUpperCase().replaceAll("[^A-Z]", "");
-        String tmp = new StringBuilder(str).reverse().toString();
-
-        if (str.equals(tmp)) answer = "YES";
+    public int solution(String str) {
+        int answer = 0;
+        char[] ch = str.toCharArray();
+        
+        for (char x : ch) {
+            if (x >= 48 && x <= 57) answer = answer*10 + (x-48);
+        }
 
         return answer;
     }
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        String str = kb.nextLine();
+        String str = kb.next();
         kb.close();
         System.out.println(T.solution(str));
     }
