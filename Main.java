b0f77df7cbbd4bf6d334ec1b430a4dd9978117f0
jnsndev <jnsn.yang@gmail.com>
2021-06-02 17:44:27 +0900

    [String] 문자 뒤집기 (직접 교환)

--- Main.java
+++ Main.java
@@ -1,15 +1,39 @@
 import java.util.*;
  
 /**
- * 단어 뒤집기 (StringBuilder의 reverse 사용)
+ * 단어 뒤집기 (직접 교환)
  */ 
 public class Main {
     public ArrayList<String> solution(String[] words) {
         ArrayList<String> answer = new ArrayList<>();
 
+        // 방법1
+        // for (String str : words) {
+        //     int len = str.length();
+        //     char[] ch = str.toCharArray();
+
+        //     // len = 4인 경우 첫번째 문자와 맨 뒤의 문자, 두번째 문자와 세번째 문자를 바꿔주면 됨 => 총 2회
+        //     for (int i=0; i<len/2; i++) {
+        //         char tmp = ch[i];
+        //         ch[i] = ch[len-i-1];
+        //         ch[len-i-1] = tmp;
+        //     }
+        //     answer.add(String.valueOf(ch));
+        // }
+
+        // 방법2
         for (String str : words) {
-            String sb = new StringBuilder(str).reverse().toString();
-            answer.add(sb);
+            char[] ch = str.toCharArray();
+            int lt = 0, rt = ch.length-1;
+
+            while (lt < rt) {
+                char tmp = ch[lt];
+                ch[lt] = ch[rt];
+                ch[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            answer.add(String.valueOf(ch));
         }
 
         return answer;
