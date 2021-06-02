d005da886353785e75f98b25450e0e134db55f49
jnsndev <jnsn.yang@gmail.com>
2021-06-02 21:11:12 +0900

    [String] 회문문자열 (원소 직접 비교)

--- Main.java
+++ Main.java
@@ -1,14 +1,28 @@
 import java.util.*;
  
 /**
- * 중복문자 제거 (reverse, equalsIgnoreCase)
+ * 회문문자열 (원소 직접 비교)
  */ 
 public class Main {
     public String solution(String str) {
-        String answer = "NO";
-        String tmp = new StringBuilder(str).reverse().toString();
+        String answer = "YES";
+        str = str.toUpperCase();
 
-        if (str.equalsIgnoreCase(tmp)) answer = "YES";
+        // 방법1
+        // int lt = 0, rt = str.length()-1;
+
+        // while (lt < rt) {
+        //     if (str.charAt(lt) != str.charAt(rt)) answer = "NO";
+        //     lt++;
+        //     rt--;
+        // }
+
+        // 방법2
+        int len = str.length();
+
+        for (int i=0; i<len/2; i++) {
+            if (str.charAt(i) != str.charAt(len-i-1)) answer = "NO";
+        }
 
         return answer;
     }
