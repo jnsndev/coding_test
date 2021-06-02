8a2c21b3128dc483a77fdb21f4eb363e68020526
jnsndev <jnsn.yang@gmail.com>
2021-06-02 22:09:34 +0900

    [String] 유효한 팰린드롬 (replaceAll, 정규식)

--- Main.java
+++ Main.java
@@ -1,35 +1,22 @@
 import java.util.*;
  
 /**
- * 회문문자열 (원소 직접 비교)
+ * 유효한 팰린드롬 (replaceAll)
  */ 
 public class Main {
     public String solution(String str) {
-        String answer = "YES";
-        str = str.toUpperCase();
+        String answer = "NO";
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+        String tmp = new StringBuilder(str).reverse().toString();
 
-        // 방법1
-        // int lt = 0, rt = str.length()-1;
-
-        // while (lt < rt) {
-        //     if (str.charAt(lt) != str.charAt(rt)) answer = "NO";
-        //     lt++;
-        //     rt--;
-        // }
-
-        // 방법2
-        int len = str.length();
-
-        for (int i=0; i<len/2; i++) {
-            if (str.charAt(i) != str.charAt(len-i-1)) answer = "NO";
-        }
+        if (str.equals(tmp)) answer = "YES";
 
         return answer;
     }
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        String str = kb.next();
+        String str = kb.nextLine();
         kb.close();
         System.out.println(T.solution(str));
     }
