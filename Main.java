ddab2e817a0cb6f7bd3cff6032b2799112cc19de
jnsndev <jnsn.yang@gmail.com>
2021-06-05 18:05:54 +0900

    [String] 문자열 압축

--- Main.java
+++ Main.java
@@ -1,31 +1,20 @@
 import java.util.*;
  
 /**
- * 가장 짧은 문자거리
+ * 문자열 압축
  */ 
 public class Main {
-    public int[] solution(String str, char t) {
-        int answer[] = new int[str.length()];
-        int p = 1000;
+    public String solution(String str) {
+        String answer = "";
+        str = str + " ";
+        int cnt = 1;
 
-        // 해당 문자의 왼쪽에 위치한 t를 기준으로 거리 구하기
-        for (int i=0; i<str.length(); i++) {
-            if (str.charAt(i) == t) {
-                p = 0;
-                answer[i] = p;
-            } else {
-                p++;
-                answer[i] = p;
-            }
-        }
-
-        // 해당 문자의 오른쪽에 위치한 t를 기준으로 거리를 구한 후, 앞서 구했던 거리와 비교하여 최소값을 저장
-        p = 1000;
-        for (int i=str.length()-1; i>=0; i--) {
-            if (str.charAt(i) == t) p = 0;
+        for (int i=0; i<str.length()-1; i++) {
+            if (str.charAt(i) == str.charAt(i+1)) cnt++;
             else {
-                p++;
-                answer[i] = Math.min(p, answer[i]);
+                answer += str.charAt(i);
+                if (cnt > 1) answer += String.valueOf(cnt);
+                cnt = 1;
             }
         }
 
@@ -35,8 +24,7 @@ public class Main {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         String str = kb.next();
-        char t = kb.next().charAt(0);
         kb.close();
-        for (int x : T.solution(str, t)) System.out.print(x + " ");
+        System.out.println(T.solution(str));
     }
 }
