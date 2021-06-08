c48931e19229c24d278b08e4dfd4b5743b86090d
jnsndev <jnsn.yang@gmail.com>
2021-06-09 02:24:36 +0900

    [DFS] 피보나치 수열 재귀

--- Main.java
+++ Main.java
@@ -1,20 +1,23 @@
 import java.util.*;
  
 /**
- * 팩토리얼
+ * 피보나치 수열 재귀
  */ 
 public class Main {
-    static int answer = 1;
+    static int[] arr;
 
     public int DFS(int n) {
-        if (n == 1) return 1;
-        else return n * DFS(n-1);
+        if (arr[n] > 0) return arr[n];  // 같은 노드를 재방문하지 않기 위한 조건문
+        if (n == 1 || n == 2) return arr[n] = 1;
+        else return arr[n] = DFS(n-2) + DFS(n-1);
     }
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
         kb.close();
-        System.out.println(T.DFS(n));
+        arr = new int[n+1];
+        T.DFS(n);
+        for (int i=1; i<=n; i++) System.out.print(arr[i] + " ");
     }
 }
