b4beaa2fd5e473e14d80427b51db4174b2e5417d
jnsndev <jnsn.yang@gmail.com>
2021-06-11 19:13:37 +0900

    [DFS] 부분집합 구하기

--- Main.java
+++ Main.java
@@ -1,39 +1,35 @@
 import java.util.*;
  
 /**
- * 이진트리 순회 (깊이우선탐색)
+ * 부분집합 구하기
  */ 
-class Node {
-    int data;
-    Node lt, rt;
-    public Node(int val) {
-        data = val;
-        lt=rt=null;
-    }
-}
-
 public class Main {
-    Node root;
+    static int n;
+    static int[] arr;
 
-    public void DFS(Node root) {
-        if (root == null) return;
-        else {
-            // System.out.print(root.data + " ");  // 전위순회
-            DFS(root.lt);
-            // System.out.print(root.data + " ");  // 중위순회
-            DFS(root.rt);
-            System.out.print(root.data + " ");  // 후위순회
+    public void DFS(int L) {
+        if (L == n+1) {
+            String tmp = "";
+            for (int i=1; i<=n; i++) {
+                if (arr[i] == 1) tmp += i + " ";
+            }
+            // 공집합을 출력하지 않기 위한 조건문
+            if (tmp.length() > 0) System.out.println(tmp);
+        } else {
+            // L을 부분집합 원소로 사용
+            arr[L] = 1;
+            DFS(L+1);
+            // L을 부분집합 원소로 사용하지않음
+            arr[L] = 0;
+            DFS(L+1);
         }
     }
     public static void main(String[] args) {
-        Main tree = new Main();
-        tree.root = new Node(1);
-        tree.root.lt = new Node(2);
-        tree.root.rt = new Node(3);
-        tree.root.lt.lt = new Node(4);
-        tree.root.lt.rt = new Node(5);
-        tree.root.rt.lt = new Node(6);
-        tree.root.rt.rt = new Node(7);
-        tree.DFS(tree.root);
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        arr = new int[n+1];
+        kb.close();
+        T.DFS(1);
     }
 }
