7375c35421811d98cb14640b7bc47383a3cd03c4
jnsndev <jnsn.yang@gmail.com>
2021-06-16 12:16:17 +0900

    [DFS] 경로 탐색 (인접행렬)

--- Main.java
+++ Main.java
@@ -1,32 +1,42 @@
 import java.util.*;
  
 /**
- * tree 말단 노드까지의 가장 짧은 경로
+ * 경로 탐색 (인접행렬)
  */ 
-class Node {
-    int data;
-    Node lt, rt;
-
-    public Node(int val) {
-        data = val;
-        lt = rt = null;
-    }
-}
-
 public class Main {
-    Node root;
+    static int n, m, answer;    // 정점의 수, 간선의 수, 최종 값
+    static int[] ch;            // 노드 방문여부 체크
+    static int[][] graph;       // 연결정보 표현
 
-    public int DFS(int level, Node root) {
-        if (root.lt == null && root.rt == null) return level;
-        else return Math.min(DFS(level+1, root.lt), DFS(level+1, root.rt));
+    public void DFS(int v) {
+        if (v == n) answer++;
+        else {
+            for (int i=1; i<=n; i++) {
+                if (graph[v][i] == 1 && ch[i] == 0) {   // 연결정보에 부합 && 방문한 적 없는 노드
+                    ch[i] = 1;
+                    DFS(i);
+                    ch[i] = 0;           
+                }
+            }
+        }
     }
     public static void main(String[] args) {
-        Main tree = new Main();
-        tree.root = new Node(1);
-        tree.root.lt = new Node(2);
-        tree.root.rt = new Node(3);
-        tree.root.lt.lt = new Node(4);
-        tree.root.lt.rt = new Node(5);
-        System.out.println(tree.DFS(0, tree.root));
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        ch = new int[n+1];
+        graph = new int[n+1][n+1];
+    
+        for (int i=0; i<m; i++) {
+            int a = kb.nextInt();
+            int b = kb.nextInt();
+            graph[a][b] = 1;
+        }
+        kb.close();
+
+        ch[1] = 1;
+        T.DFS(1);
+        System.out.println(answer);
     }
 }
