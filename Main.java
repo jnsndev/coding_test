813d37fcc8c54db449dc9e1f62ba301a0839c293
jnsndev <jnsn.yang@gmail.com>
2021-06-18 23:47:47 +0900

    [DFS] 경로 탐색 (인접리스트)

--- Main.java
+++ Main.java
@@ -1,40 +1,45 @@
 import java.util.*;
  
 /**
- * 경로 탐색 (인접행렬)
+ * 경로 탐색 (인접리스트)
  */ 
 public class Main {
-    static int n, m, answer;    // 정점의 수, 간선의 수, 최종 값
-    static int[] ch;            // 노드 방문여부 체크
-    static int[][] graph;       // 연결정보 표현
+    static int n, m, answer;
+    static int[] ch;
+    static List<ArrayList<Integer>> list;
 
     public void DFS(int v) {
         if (v == n) answer++;
         else {
-            for (int i=1; i<=n; i++) {
-                if (graph[v][i] == 1 && ch[i] == 0) {   // 연결정보에 부합 && 방문한 적 없는 노드
-                    ch[i] = 1;
-                    DFS(i);
-                    ch[i] = 0;           
+            for (int nv : list.get(v)) {
+                if (ch[nv] == 0) {
+                    ch[nv] = 1;
+                    DFS(nv);
+                    ch[nv] = 0;
                 }
             }
         }
     }
+    
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         n = kb.nextInt();
         m = kb.nextInt();
-        ch = new int[n+1];
-        graph = new int[n+1][n+1];
-    
+        list = new ArrayList<ArrayList<Integer>>();
+
+        for (int i=0; i<=n; i++) {
+            list.add(new ArrayList<Integer>());
+        }
+
         for (int i=0; i<m; i++) {
             int a = kb.nextInt();
             int b = kb.nextInt();
-            graph[a][b] = 1;
+            list.get(a).add(b);
         }
         kb.close();
 
+        ch = new int[n+1];
         ch[1] = 1;
         T.DFS(1);
         System.out.println(answer);
