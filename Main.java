93deb8ed48a6f7737e5816ca0a2671019518cf80
jnsndev <jnsn.yang@gmail.com>
2021-06-27 16:44:05 +0900

    [DFS] 순열 구하기

--- Main.java
+++ Main.java
@@ -1,20 +1,24 @@
 import java.util.*;
  
 /**
- * 동전교환
+ * 순열 구하기
  */ 
 public class Main {
-    static int n, total, answer = Integer.MAX_VALUE;
-    static int[] coin;
+    static int n, m;   // n: n개의 자연수 m: 뽑는 횟수
+    static int[] nums, ch, answer;
 
-    public void DFS(int level, int price) {
-        if (price > total) return;
-        if (level >= answer) return;
-        if (price == total) {
-            answer = Math.min(answer, level);
+    public void DFS(int level) {
+        if (level == m) {
+            for (int x : answer) System.out.print(x + " ");
+            System.out.println();
         } else {
-            for (int i=0; i<coin.length; i++) {
-                DFS(level+1, price+coin[i]);
+            for (int i=0; i<n; i++) {
+                if (ch[i] == 0) {   // 원소 방문 여부 체크
+                    ch[i] = 1;
+                    answer[level] = nums[i];
+                    DFS(level+1);
+                    ch[i] = 0;
+                }
             }
         }
     }
@@ -23,13 +27,15 @@ public class Main {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         n = kb.nextInt();
-        coin = new int[n];
-        for (int i=n-1; i>=0; i--) {
-            coin[i] = kb.nextInt();
+        m = kb.nextInt();
+        nums = new int[n];
+        ch = new int[n];
+        answer = new int[m];
+
+        for (int i=0; i<n; i++) {
+            nums[i] = kb.nextInt();           
         }
-        total = kb.nextInt();
         kb.close();
-        T.DFS(0, 0);
-        System.out.println(answer);
+        T.DFS(0);
     }
 }
