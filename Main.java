327c82d8a201075cea2b76a67b770e1e93c56f62
jnsndev <jnsn.yang@gmail.com>
2021-06-28 00:07:43 +0900

    [Array] 큰 수 출력하기

--- Main.java
+++ Main.java
@@ -1,41 +1,29 @@
 import java.util.*;
  
 /**
- * 순열 구하기
+ * 큰 수 출력하기
  */ 
 public class Main {
-    static int n, m;   // n: n개의 자연수 m: 뽑는 횟수
-    static int[] nums, ch, answer;
-
-    public void DFS(int level) {
-        if (level == m) {
-            for (int x : answer) System.out.print(x + " ");
-            System.out.println();
-        } else {
-            for (int i=0; i<n; i++) {
-                if (ch[i] == 0) {   // 원소 방문 여부 체크
-                    ch[i] = 1;
-                    answer[level] = nums[i];
-                    DFS(level+1);
-                    ch[i] = 0;
-                }
-            }
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for (int i=1; i<n; i++) {
+            if (arr[i] > arr[i-1]) answer.add(arr[i]);
         }
+
+        return answer;
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        n = kb.nextInt();
-        m = kb.nextInt();
-        nums = new int[n];
-        ch = new int[n];
-        answer = new int[m];
+        int n = kb.nextInt();
+        int[] arr = new int[n];
 
         for (int i=0; i<n; i++) {
-            nums[i] = kb.nextInt();           
+            arr[i] = kb.nextInt();
         }
         kb.close();
-        T.DFS(0);
+        for (int x : T.solution(n, arr)) System.out.print(x + " ");
     }
 }
