f119e65639aa0a1ed0d9c282011244550983527d
jnsndev <jnsn.yang@gmail.com>
2021-06-30 22:49:39 +0900

    [Array] 피보나치 수열

--- Main.java
+++ Main.java
@@ -1,18 +1,15 @@
 import java.util.*;
  
 /**
- * 가위바위보
+ * 피보나치 수열
  */ 
 public class Main {
-    public String solution(int n, int[] a, int[] b) {
-        String answer = "";
-        for (int i=0; i<n; i++) {
-            if (a[i] == b[i]) answer += "D";
-            else if (a[i] == 1 && b[i] == 3) answer += "A";
-            else if (a[i] == 2 && b[i] == 1) answer += "A";
-            else if (a[i] == 3 && b[i] == 2) answer += "A";
-            else answer += "B";
-        }
+    public int[] solution(int n) {
+        int[] answer = new int[n];
+        answer[0] = 1;
+        answer[1] = 1;
+
+        for (int i=2; i<n; i++) answer[i] = answer[i-1] + answer[i-2];
 
         return answer;
     }
@@ -21,18 +18,8 @@ public class Main {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        int[] a = new int[n];
-        int[] b = new int[n];
-
-        for (int i=0; i<n; i++) {
-            a[i] = kb.nextInt();
-        }
-
-        for (int i=0; i<n; i++) {
-            b[i] = kb.nextInt();
-        }
         kb.close();
 
-        for (char x : T.solution(n, a, b).toCharArray()) System.out.println(x);;
+        for (int x : T.solution(n)) System.out.print(x + " ");
     }
 }
