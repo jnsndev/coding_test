9c7fa8151025af81c56f316c9b4a03f885aff166
jnsndev <jnsn.yang@gmail.com>
2021-07-01 09:51:51 +0900

    [Array] 소수 (에라토스테네스 체)

--- Main.java
+++ Main.java
@@ -1,15 +1,29 @@
 import java.util.*;
  
 /**
- * 피보나치 수열
+ * 소수 (에라토스테네스 체)
  */ 
 public class Main {
-    public int[] solution(int n) {
-        int[] answer = new int[n];
-        answer[0] = 1;
-        answer[1] = 1;
+    public int solution(int n, int[] arr) {
+        int answer = 0;
 
-        for (int i=2; i<n; i++) answer[i] = answer[i-1] + answer[i-2];
+        // time limit exceeded
+        // for (int i=2; i<=n; i++) {
+        //     if (arr[i] == 0) {
+        //         answer++;
+        //         for (int j=2; j<=n; j++) {
+        //             if (j % i == 0) arr[j] = 1;
+        //         }
+        //     }
+        // }
+
+        for (int i=2; i<=n; i++) {
+            if (arr[i] == 0) {
+                answer++;
+                // i의 배수를 소거하는 방법
+                for (int j=i; j<=n; j=i+j) arr[j] = 1;
+            }
+        }
 
         return answer;
     }
@@ -19,7 +33,7 @@ public class Main {
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
         kb.close();
-
-        for (int x : T.solution(n)) System.out.print(x + " ");
+        int[] arr = new int[n+1];
+        System.out.println(T.solution(n, arr));
     }
 }
