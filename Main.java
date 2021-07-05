c1df123586f46d18ff6e8fc42f71b68176a3aa5a
jnsndev <jnsn.yang@gmail.com>
2021-07-05 11:56:35 +0900

    [Array] 임시반장 정하기

--- Main.java
+++ Main.java
@@ -1,31 +1,30 @@
 import java.util.*;
  
 /**
- * 봉우리 지역 개수
+ * 임시반장 정하기
  */ 
 public class Main {
-    int[] dx = {0, 1, 0, -1};
-    int[] dy = {-1, 0, 1, 0};
-
     public int solution(int n, int[][] board) {
-        int answer = 0;
+        int answer = 0, max = Integer.MIN_VALUE;
 
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                boolean flag = true;
-                int cur = board[i][j];
+        for (int i=1; i<=n; i++) {  // 비교 기준이 되는 i번 학생
+            // i번 학생의 반과 1번에서 n번 학생의 반을 비교해 한번이라도 같은 반이었던 학생 수 
+            int cnt = 0;
 
-                // 상하좌우 좌표값을 현재 좌표값과 비교
-                for (int k=0; k<4; k++) {
-                    int nx = i + dx[k];
-                    int ny = j + dy[k];
-
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] >= cur) {
-                        flag = false;
+            for (int j=1; j<=n; j++) {  // 비교 대상이 되는 j번 학생
+                for (int k=1; k<=5; k++) {  // k학년
+                    if (board[i][k] == board[j][k]) {
+                        cnt++;
+                        // i번 학생과 j번 학생이 k학년에서 같은 반인 경우
+                        // 예를 들어, k+1학년 때 다시 같은 반이 될 경우 j번 학생이 더블체크 되므로 break문 필수 
                         break;
                     }
                 }
-                if (flag) answer++;
+            }
+            
+            if (cnt > max) {
+                max = cnt;
+                answer = i;
             }
         }
 
@@ -36,10 +35,10 @@ public class Main {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        int[][] board = new int[n][n];
+        int[][] board = new int[n+1][6];
 
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=5; j++) {
                 board[i][j] = kb.nextInt();
             }
         }
