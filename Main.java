1eb3cd87d4a72dffeae1cb049626c17a3940d7c2
jnsndev <jnsn.yang@gmail.com>
2021-07-07 23:51:47 +0900

    [Two pointers] 두 배열 합치기

--- Main.java
+++ Main.java
@@ -8,19 +8,20 @@ public class Main {
         ArrayList<Integer> answer = new ArrayList<>();
         int p1 = 0, p2 = 0;
 
+        // 주어진 배열 오름차순 정렬
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
         while (p1 < n && p2 < m) {
-            if (arr1[p1] < arr2[p2]) {
+            if (arr1[p1] == arr2[p2]) {
                 answer.add(arr1[p1]);
-                p1++;
-            } else {
-                answer.add(arr2[p2]);
                 p2++;
             }
+            // 각 배열이 오름차순으로 정렬된 상태 =>
+            // arr1[p1]이 arr2[p2]보다 작다면 더 큰 수인 arr1[p1+1]과 arr2[p2]를 비교해야하므로 p1++;
+            else if (arr1[p1] < arr2[p2]) p1++;
+            else p2++;
         }
-
-        // 길이가 더 긴 배열의 남은 원소들을 출력
-        while (p1 < n) answer.add(arr1[p1++]);
-        while (p2 < m) answer.add(arr2[p2++]);
 
         return answer;
     }
