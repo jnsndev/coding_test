44d655e355c8db685cc39e773001a04513184350
jnsndev <jnsn.yang@gmail.com>
2021-07-15 19:58:20 +0900

    [Two Pointers] 최대 길이 연속부분수열

--- Main.java
+++ Main.java
@@ -1,17 +1,19 @@
 import java.util.*;
  
 /**
- * 연속된 자연수의 합
+ * 최대 길이 연속부분수열
  */ 
 public class Main {
-    public int solution(int n) {
-        int answer = 0, connectedNumCnt = 1;
+    public int solution(int n, int k, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
         
-        n--;
-        while (n > 0) {
-            connectedNumCnt++;
-            n = n - connectedNumCnt;
-            if (n % connectedNumCnt == 0) answer++;
+        for (int rt=0; rt<n; rt++) {
+            if (arr[rt] == 0) cnt++;    // 0을 1로 변경한 횟수 카운팅
+            while (cnt > k) {
+                if (arr[lt] == 0) cnt--;    // 0을 1로 변경할 수 있는 최대 횟수인 k를 넘어가면 감소
+                lt++;
+            }
+            answer = Math.max(answer, rt-lt+1);     // 1로만 구성된 연속부분수열의 최대 길이
         }
 
         return answer;
@@ -20,8 +22,11 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
+        int n = kb.nextInt();   // 수열의 길이
+        int k = kb.nextInt();   // 0을 1로 변경할 수 있는 최대 횟수
+        int[] arr = new int[n]; // 수열
+        for (int i=0; i<n; i++) arr[i] = kb.nextInt();
         kb.close();
-        System.out.println(T.solution(n));
+        System.out.println(T.solution(n, k, arr));
     }
 }
